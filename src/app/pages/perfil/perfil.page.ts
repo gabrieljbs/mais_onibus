b5432904213c27ee0a2131b35d924e5e416c3be1
gabriel <gabrieljbs@gmail.com>
2023-03-22 15:25:31 -0300

    list

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
@@ -7,9 +7,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  public jogador = {
+    nome: "Gabriel",
+    hab: 6,
+    stamina: 24
+  };
 
-  ngOnInit() {
+  public adversario = {
+    nome: "Bixaaaa",
+    hab: 6,
+    stamina: 24
+  };
+  
+  constructor(private alertController: AlertController) { }
+
+  ngOnInit(
+    max=7,
+    min=1
+  ) {
+
+    const dadoJ = Math.floor(Math.random() * (max - min) + min)
+    const dadoA = Math.floor(Math.random() * (max - min) + min)
+
+    console.log(dadoJ, dadoA);
+
+    this.jogador.hab = this.jogador.hab + dadoJ;
+    this.adversario.hab = this.adversario.hab + dadoA;
+
+
+    for (let index = 0; index < 2; index++) { 
+       this.jogador.stamina = this.jogador.stamina + Math.floor(Math.random() * (max - min) + min)
+       this.adversario.stamina = this.adversario.stamina + Math.floor(Math.random() * (max - min) + min)
+    }
+
+
+  }
+  
+  async jogar(max=7,min=1, pontoA:number, pontoJ:number){
+   
+    const dadoJ = Math.floor(Math.random() * (max - min) + min);
+    const dadoA = Math.floor(Math.random() * (max - min) + min);
+
+    pontoA = this.adversario.hab + dadoA;
+    pontoJ = this.jogador.hab + dadoJ;
+
+    if(pontoA < pontoJ){
+      this.adversario.stamina = this.adversario.stamina - 2
+    }if (pontoA > pontoJ) {
+      this.jogador.stamina = this.jogador.stamina - 2
+    } else {
+      this.jogador.stamina = this.jogador.stamina - 1
+      this.adversario.stamina = this.adversario.stamina - 1
+    }
+
+
+    console.log("pontos do Adversario: "+pontoA, "pontos do jogador: "+pontoJ)
+
+    if(this.jogador.stamina <= 0){
+      const alert = await this.alertController.create({
+        header: 'Game Over',
+        subHeader: 'Você perdeu',
+        message: 'tente de novo OTARIO, CLICK QUE SE VOCÊ É    UMA BIXINHA',
+        buttons: ['OK'],
+        
+        });
+        await alert.present();
+
+
+    }if (this.adversario.stamina <= 0){ 
+      const alert1 = await this.alertController.create({
+        header: 'Game Over',
+        subHeader: 'Você ganhou',
+        message: 'Parabens!',
+        buttons: ['OK'],
+        });
+        await alert1.present();
+    } else {
+      
+    }
+      
+
   }
 
 }
